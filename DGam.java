3d45d0e26465dd90fbe153d35e66e0e6096cabed
Aiman Rattab <45946777+Hightness@users.noreply.github.com>
2024-03-19 15:46:02 +0100

    Add files via upload

---
+++ DGam.java
@@ -0,0 +1,11 @@
+
+public class DGam {
+
+	public static void main(String[] args) {
+		GamePanel pane1=new GamePanel();
+		GamePanel pane2=new GamePanel();
+		GameFrame frame=new GameFrame();
+		frame.add(pane1);
+	}
+
+}
